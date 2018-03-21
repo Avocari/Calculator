5badb2f15e8f6864a1ecb43253553940c0ad75aa
Alexander Hiebl <hiebl@student.tugraz.at>
2018-03-21 08:50:39 +0100

    Imports

--- app/src/androidTest/java/at/sw2018/calculator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2018/calculator/ExampleInstrumentedTest.java
@@ -2,11 +2,16 @@ package at.sw2018.calculator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
